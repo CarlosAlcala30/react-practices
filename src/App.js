e1ffee0f8259d8b0c10abc95be86b00cd623242f
CarlosAlcala <alcala1500@gmail.com>
2021-09-03 22:24:04 -0500

    logica terminada

--- src/App.js
+++ src/App.js
@@ -1,126 +1,88 @@
 import React, { useState, useEffect } from 'react'
+
+import "./App.scss"
+
 import {
   Container,
   Row,
   Col,
-  Table,
   Button,
+  Label,
+  Input,
+  Form,
+  FormGroup,
+  
 } from 'reactstrap'
 
-//import UsersForm from './Components/UsersForm'
 
-//import UsersTable from './Components/UsersTable'
-
-import data from './mockData/index'
 
 const App = () => {
-  const [users, setUsers] = useState()
-  const [userData, setUserData] = useState({})
-  const [filterResult, setFilterResult ] = useState([])
-  const [listProducts, setListProducts ] = useState(data.products)
-  const [buyList, setBuyList ] = useState([])
-  const [total, setTotal ] = useState(0)
+  const [bill, setBill] = useState(0)
+  const [tip, setTip] = useState(0)
+  const [people, setPeople] = useState(0)
 
-  const userHandler = event => {
-    const property = event.target.name
+  const billHandler = event => {
+    const price = event.target.value
+    setBill(price)
+  }
+
+  const tipHandler = event => {
+    const tip = event.target.dataset.tip
+    setTip(tip);
+  }
+
+  const peopleHandler = event => {
     const value = event.target.value
-    setUserData({ ...userData, [property]: value })
-    console.log(userData)
+    setPeople(value);
   }
 
-  const saveUser = () => {
-    !users ? setUsers([userData]) : setUsers([...users, userData])
+  const resetHandler = event => {
+    setBill(0);
+    setTip(0);
+    setPeople(0);
   }
 
-  const filterHandler = event => {
-    const data = users
-    const value = event.target.value
-
-    const result = data.filter( user => user.nombre.toLowerCase().includes( value.toLowerCase()))
-    setFilterResult( result )
-  }
-
-  const addHandler = (event) => {
-    const index = event.target.dataset.index 
-    const price =  listProducts[index].price
-    setBuyList([...buyList,listProducts[index]])       
-    setTotal(total+price)
-  }
-
-  const removeHandler = (event) => {
-    const index = event.target.dataset.index 
-    const price =  buyList[index].price
-    buyList.splice( index,1 )
-    setBuyList(buyList)
-    setTotal(total-price)
-  }
 
 
   return (
     <>
-      <Container>
-        <Row>
-          <Col md="6">
-            <Table dark>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Product Name</th>
-                  <th>Price</th>
-                  <th>Description</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  listProducts.map( (product,index) => {
-                    const {name, price, description} = product
-                    return (
-                      <tr key={index}>
-                        <td >{index}</td>
-                        <td >{name}</td>
-                        <td>{price}</td>
-                        <td>{description}</td>
-                        <td><Button color="success" size="md" active  data-index={index} onClick={addHandler}>ADD</Button></td>
-                      </tr>
-                    )
-                  })
-                }          
-               
-              </tbody>
-            </Table>
+      <Container fluid className="bg-container d-flex flex-column align-items-center"> 
+        <div class="cal-title"><h1>SPLI<br/>TTER</h1></div>
+        <Row className="cal-container">
+          <Col md="6" className="">            
+          <Form>
+            <FormGroup>
+              <Label for="bill-input">Bill</Label>
+              <Input type="number" class="text-right" name="bill" id="bill-input" placeholder="$" onChange={billHandler} value={bill}/>
+            </FormGroup>
+            <FormGroup>
+              <Label>Select tip %</Label>
+              <Button type="button"  data-tip=".05" onClick={tipHandler}>5%</Button>
+              <Button type="button"  data-tip=".10" onClick={tipHandler}>10%</Button>
+              <Button type="button" data-tip=".15" onClick={tipHandler}>15%</Button>
+              <Button type="button"  data-tip=".25" onClick={tipHandler}>25%</Button>
+              <Button type="button"  data-tip=".50" onClick={tipHandler}>50%</Button>
+              <Input type="number" min="0" placeholder="CUSTOM"/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="people-input">Number of People</Label>
+              
+              <Input type="number" class="text-right" name="people" id="people-input" placeholder="&#61442;" onChange={peopleHandler} value={people} />
+            </FormGroup>
+          </Form>
           </Col>
-          <Col md="6">
-          <Table dark>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Product Name</th>
-                  <th>Price</th>
-                  <th>Description</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  buyList.map((product,index) => {
-                    const {name, price, description} = product
-                    return (
-                      <tr key={index}>
-                        <td >{index}</td>
-                        <td >{name}</td>
-                        <td>{price}</td>
-                        <td>{description}</td>
-                        <td><Button color="danger" size="md" active  data-index={index} onClick={removeHandler}>REMOVE</Button></td>                        
-                      </tr>
-                    )
-                  })
-                }           
-               
-              </tbody>
-            </Table>
-            <h1>Total: {total}</h1>
-
+          <Col md="6" className="print-total">
+            <div className="d-flex justify-content-between">
+              <p className="col-6">Tip Amount<br/><span>/person</span></p>
+              <h2 className="col-6"id="tip-total">${bill?((bill*tip)/people):0}</h2>
+            </div> 
+            <div className="d-flex">
+              <p className="col-6">Total<br/><span>/person</span></p>
+              <h2 className="col-6" id="total">${bill?((bill*(tip+1))/people):0}</h2>
+            </div>
+            <div className="d-flex justify-content-center">
+              <Button type="input" size="lg" onClick={resetHandler}>RESET</Button>
+            </div>
           </Col>
         </Row>
       </Container>
