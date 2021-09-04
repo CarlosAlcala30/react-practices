86a4555caba476986a027bde596eeb09c4183c43
CarlosAlcala <alcala1500@gmail.com>
2021-09-03 19:38:20 -0500

    carrito terminado

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import {
   Container,
   Row,
   Col,
+  Table,
+  Button,
 } from 'reactstrap'
-import UsersForm from './Components/UsersForm'
 
-import UsersTable from './Components/UsersTable'
+//import UsersForm from './Components/UsersForm'
+
+//import UsersTable from './Components/UsersTable'
+
+import data from './mockData/index'
 
 const App = () => {
   const [users, setUsers] = useState()
   const [userData, setUserData] = useState({})
   const [filterResult, setFilterResult ] = useState([])
+  const [listProducts, setListProducts ] = useState(data.products)
+  const [buyList, setBuyList ] = useState([])
+  const [total, setTotal ] = useState(0)
 
   const userHandler = event => {
     const property = event.target.name
@@ -32,23 +40,87 @@ const App = () => {
     setFilterResult( result )
   }
 
+  const addHandler = (event) => {
+    const index = event.target.dataset.index 
+    const price =  listProducts[index].price
+    setBuyList([...buyList,listProducts[index]])       
+    setTotal(total+price)
+  }
+
+  const removeHandler = (event) => {
+    const index = event.target.dataset.index 
+    const price =  buyList[index].price
+    buyList.splice( index,1 )
+    setBuyList(buyList)
+    setTotal(total-price)
+  }
+
+
   return (
     <>
       <Container>
         <Row>
-          <Col xs="12" md="3">
-            <UsersForm 
-              userHandler = { userHandler }
-              saveUser = { saveUser }
-            />
+          <Col md="6">
+            <Table dark>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Product Name</th>
+                  <th>Price</th>
+                  <th>Description</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  listProducts.map( (product,index) => {
+                    const {name, price, description} = product
+                    return (
+                      <tr key={index}>
+                        <td >{index}</td>
+                        <td >{name}</td>
+                        <td>{price}</td>
+                        <td>{description}</td>
+                        <td><Button color="success" size="md" active  data-index={index} onClick={addHandler}>ADD</Button></td>
+                      </tr>
+                    )
+                  })
+                }          
+               
+              </tbody>
+            </Table>
           </Col>
-          <Col xs="12" md="9">
-            {users && 
-              <UsersTable 
-                usersList={ filterResult.length ? filterResult : users } 
-                filterHandler = { filterHandler }
-              />
-            }
+          <Col md="6">
+          <Table dark>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Product Name</th>
+                  <th>Price</th>
+                  <th>Description</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  buyList.map((product,index) => {
+                    const {name, price, description} = product
+                    return (
+                      <tr key={index}>
+                        <td >{index}</td>
+                        <td >{name}</td>
+                        <td>{price}</td>
+                        <td>{description}</td>
+                        <td><Button color="danger" size="md" active  data-index={index} onClick={removeHandler}>REMOVE</Button></td>                        
+                      </tr>
+                    )
+                  })
+                }           
+               
+              </tbody>
+            </Table>
+            <h1>Total: {total}</h1>
+
           </Col>
         </Row>
       </Container>
